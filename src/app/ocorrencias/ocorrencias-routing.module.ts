d6160d7dfeb75c1361dbf529f7c741c7c76ce198
Alexandre <alexandre.trieste@gmail.com>
2022-03-11 16:19:21 -0300

    estabelecendo rotas de ocorrências

--- src/app/ocorrencias/ocorrencias-routing.module.ts
+++ src/app/ocorrencias/ocorrencias-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OcorrenciasComponent } from './ocorrencias/ocorrencias.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component: OcorrenciasComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
